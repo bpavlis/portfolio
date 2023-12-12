dd7d6e189204e4a5446b673a2715a3bda4b39fa9
bpavlis <benpavlis@gmail.com>
2023-12-11 18:39:25 -0600

    completed resume page and started on contact

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -2,12 +2,51 @@ import { useState, useEffect } from "react"
 // import './App.css'
 
 export default function ContactMe(props){
+  const [userData, setUserData] = useState("")
+  const [userName, setUserName] = useState("")
+  const [userEmail, setUserEmail] = useState("")
+  const [userMessage, setUserMessage] = useState("")
+
+  function handleInputChange(e){
+    setUserData({...userData, [e.target.name]: e.target.value })
+  }
+
+  async function handleFormSubmit(e){
+    e.preventDefault()
+
+  }
 
 
   return (
     <>
       <div>
         <h1 className="pageTitle">Contact Me</h1>
+      </div>
+      <div className="aboutContainer">
+      <div>
+      <form onSubmit={handleFormSubmit}>
+        <div>
+          <div>
+            <div>
+              <label className="d-block">Name: </label>
+              <input type="text" name="email" value={userData.email} onChange={handleInputChange} />
+            </div>
+
+            <div>
+              <label className="d-block">Email: </label>
+              <input type="text" name="email" value={userData.email} onChange={handleInputChange} />
+            </div>
+
+            <div>
+              <label className="d-block">Message: </label>
+              <input type="text" name="email" value={userData.email} onChange={handleInputChange} />
+            </div>
+          </div>
+
+          <button className="mt-2">Submit Info</button>
+        </div>
+      </form>
+    </div>
       </div>
     </>
   )
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,13 +1,32 @@
 import { useState, useEffect } from "react"
-// import './App.css'
+import resume from '../assets/BenjaminPavlisResume2023.pdf'
 
-export default function Resume(props){
+export default function Resume(props) {
 
 
   return (
     <>
       <div>
         <h1 className="pageTitle">My Resume</h1>
+      </div>
+      <div className="downloadResContainer">
+        <h2>Click Here To Download My Resume:</h2>
+        <a href={resume} download><img className="resImg" src="https://cdn-icons-png.flaticon.com/512/88/88645.png" alt="Download my resume here!" /></a>
+      </div>
+      <h2 className="profTitle">Check out my developer proficiencies:</h2>
+      <div className="resContainer">
+        <div className="frontEnd">
+          <h2>Front-End</h2>
+          <p>HTML5<br />CSS<br />JavaScript<br />API Work<br />Bootstrap<br />Handlebars<br />GIT</p>
+        </div>
+        <div className="backEnd">
+          <h2>Back-End</h2>
+          <p>Express<br />MySQL<br />Node<br />Sequelize<br />Jest<br />ORM<br />MVC</p>
+        </div>
+        <div className="performence">
+          <h2>Performence</h2>
+          <p>React<br />NoSQL<br />Mongoose<br />MongoDB<br />PWA<br />GraphQL<br />MERN<br />State</p>
+        </div>
       </div>
     </>
   )
